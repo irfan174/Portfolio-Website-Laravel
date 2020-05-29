f47559d452712256772a2653e64e4e29c3c49dbd
Irfan Ahmed <57946377+irfan174@users.noreply.github.com>
2020-05-29 23:25:31 +0600

    Service section - Admin part 02 (add loading animation)
    add a loading animation with js axios before data fetch and show on the table,
    add a something went wrong h3 when data can't be fetched.

--- admin/public/js/custom.js
+++ admin/public/js/custom.js
@@ -4,19 +4,39 @@ $('.dataTables_length').addClass('bs-select');
 });
 
 function getServiceJsonData(){
+
+
+
+
+
 	axios.get('/servicedata')
   	.then(function (response) {
-    var serviceDataJson=response.data;
-    $.each(serviceDataJson, function(i, item) {
-    $('<tr>').html(
-    	"<td <img class='table-img' src="+ serviceDataJson[i].service_img + "></td>" +
-    	"<td>" + serviceDataJson[i].service_name + "</td>" +
-    	"<td>" + serviceDataJson[i].service_des + "</td>" +
-    	"<td><a href='' ><i class='fas fa-edit'></i></a></td>" +
-    	"<td><a href='' ><i class='fas fa-trash-alt'></i></a></td>").appendTo('#serviceData_table');
-   });
+  		if(response.status==200)
+  		{
+  			$('#dataTable').removeClass('d-none');
+  			$('#loadingAnim').addClass('d-none');
+
+  			var serviceDataJson=response.data;
+    		$.each(serviceDataJson, function(i, item) {
+    		$('<tr>').html(
+    		"<td <img class='table-img' src="+ serviceDataJson[i].service_img + "></td>" +
+    		"<td>" + serviceDataJson[i].service_name + "</td>" +
+    		"<td>" + serviceDataJson[i].service_des + "</td>" +
+    		"<td><a href='' ><i class='fas fa-edit'></i></a></td>" +
+    		"<td><a href='' ><i class='fas fa-trash-alt'></i></a></td>").appendTo('#serviceData_table');
+   			});
+
+  		}
+  		else{
+  			$('#notFound').removeClass('d-none');
+  			$('#loadingAnim').addClass('d-none');
+
+  		}
+    
 
    }).catch(function (error) {
+   			$('#notFound').removeClass('d-none');
+  			$('#loadingAnim').addClass('d-none');
 
    });
 }
--- admin/resources/views/Services.blade.php
+++ admin/resources/views/Services.blade.php
@@ -2,30 +2,48 @@
 
 @section('content')
 
-<div class="container">
-<div class="row">
-<div class="col-md-12 p-5">
-<table id="" class="table table-striped table-bordered" cellspacing="0" width="100%">
-  <thead>
-    <tr>
-    <th class="th-sm">Image</th>
-    <th class="th-sm">Name</th>
-    <th class="th-sm">Description</th>
-    <th class="th-sm">Edit</th>
-    <th class="th-sm">Delete</th>
-    </tr>
-  </thead>
-  <tbody id="serviceData_table">
+<div id="dataTable" class="container d-none">
+  <div class="row">
+    <div class="col-md-12 p-5">
+        <table id="" class="table table-striped table-bordered" cellspacing="0" width="100%">
+          <thead>
+            <tr>
+            <th class="th-sm">Image</th>
+            <th class="th-sm">Name</th>
+            <th class="th-sm">Description</th>
+            <th class="th-sm">Edit</th>
+            <th class="th-sm">Delete</th>
+            </tr>
+          </thead>
+          <tbody id="serviceData_table">
 
-  
-  
-  
-  
-  </tbody>
-</table>
+          </tbody>
+        </table>
 
+    </div>
+  </div>
 </div>
+
+<div id="loadingAnim" class="container">
+  <div class="row">
+    <div class="col-md-12 text-center p-5">
+      <img class="loading-anim m-5" src="{{asset('images/loader.svg')}}" alt="">
+      
+    </div>
+    
+  </div>
+  
 </div>
+
+<div id="notFound" class="container d-none">
+  <div class="row">
+    <div class="col-md-12 text-center p-5">
+      <h3>Something Went Wrong !!!</h3>
+      
+    </div>
+    
+  </div>
+  
 </div>
 
 @endsection
